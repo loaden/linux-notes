a9bf68947cd9cea1f239549c4350a042f15f509d
Yuchen Deng <loaden@gmail.com>
2023-01-12 19:30:54 +0800

    更新切换脚本

--- .bin/chroot.sh
+++ .bin/chroot.sh
@@ -4,7 +4,9 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-mount -o subvol=@home /dev/sda2 /mnt/gentoo/home
-mount /dev/sda1 /mnt/gentoo/boot/efi
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
+mount -o subvol=@home /dev/nvme0n1p2 /mnt/gentoo/home
+mount --mkdir /dev/nvme0n1p1 /mnt/gentoo/boot/efi
 cp /etc/resolv.conf /mnt/gentoo/etc/
 chroot /mnt/gentoo /bin/bash
