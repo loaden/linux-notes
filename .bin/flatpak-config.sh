490c5707e0f0953687118f25caa74d42dc04dabb
loaden <loaden@live.com>
2026-03-19 09:20:34 +0800

    更新flatpak配置

--- .bin/flatpak-config.sh
+++ .bin/flatpak-config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # 维护：Yuchen Deng [loaden] 钉钉群：35948877
 # QQ群：19346666、111601117
+# 中科大镜像缓存
+# https://mirrors.ustc.edu.cn/help/flathub.html
+# 上海交大镜像缓存
 # https://mirrors.sjtug.sjtu.edu.cn/docs/flathub
-# 上海交大智能镜像缓存
 
-read -p "是否添加Flatpak国内源到系统目录？[y/N]" choice
+read -p "是否添加Flatpak国内源到系统目录？[y/N]" -t 5 choice
 case $choice in
     Y | y) SYSTEM_INSTALL=1 ;;
     N | n | '') SYSTEM_INSTALL=0 ;;
@@ -13,14 +15,18 @@ esac
 
 if [ "$SYSTEM_INSTALL" == "1" ]; then
     sudo flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    sudo flatpak remote-modify --system flathub --url=https://mirror.sjtu.edu.cn/flathub
+    sudo flatpak remote-modify --system flathub --url=https://mirrors.ustc.edu.cn/flathub
+    #sudo flatpak remote-modify --system flathub --url=https://mirror.sjtu.edu.cn/flathub
     sudo flatpak remote-modify --system flathub --prio=2
     sudo sed -i 's/.*url-is-set=.*/url-is-set=true/g' /var/lib/flatpak/repo/config
     [[ -z $(cat /var/lib/flatpak/repo/config | grep url-is-set=true ) ]] && sudo bash -c 'echo "url-is-set=true" >> /var/lib/flatpak/repo/config'
 fi
 
+read -p "是否添加Flatpak国内源到用户目录？[Y/n]" choice
+if [ -z "$choice" ]; then
+    choice=Y
+fi
 
-read -p "是否添加Flatpak国内源到用户目录？[y/N]" choice
 case $choice in
     Y | y) USER_INSTALL=1 ;;
     N | n | '') USER_INSTALL=0 ;;
@@ -29,10 +35,13 @@ esac
 
 if [ "$USER_INSTALL" == "1" ]; then
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak remote-modify --user flathub --url=https://mirror.sjtu.edu.cn/flathub
+    flatpak remote-modify --user flathub --url=https://mirrors.ustc.edu.cn/flathub
+    #flatpak remote-modify --user flathub --url=https://mirror.sjtu.edu.cn/flathub
     sed -i 's/.*url-is-set=.*/url-is-set=true/g' ~/.local/share/flatpak/repo/config
     [[ -z $(cat ~/.local/share/flatpak/repo/config | grep url-is-set=true ) ]] && echo "url-is-set=true" >> ~/.local/share/flatpak/repo/config
 fi
 
-
 flatpak remote-list --columns=name,title,options,priority,url
+
+# 排查网络
+# OSTREE_DEBUG_HTTP=1 flatpak install org.kde.kdenlive
